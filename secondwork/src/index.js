39b80548373338f4fe4ff2f16c2c0e3412fdfacb
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-22 09:40:20 +0200

    add navbar into header

--- secondwork/src/index.js
+++ secondwork/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {createRoot} from "react-dom/client";
+import {Application} from "./Application";
+import {Header} from "./Header";
+import './index.css'
 const root=createRoot(document.querySelector('#root'));
 
-const component=(<header>
-    Test
-</header>)
+// const component=(<header>
+//     Test
+// </header>)
 
 
 
@@ -12,7 +15,8 @@ const component=(<header>
 
 
 root.render(
-    <div>{component}
-        Hello
-    </div>
+    <Application/>
 )
+
+// Написанное с маленькой буквы воспринимается как тег, написанное с большой будет восприниматься как компонент
+// написанное в {} скобках подразумевает выражение кода написанное выше, все что без скобок будет восприниматься как строка
