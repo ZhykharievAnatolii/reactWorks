72c3ed7c574318e9090e99fced2e5dda7a77041c
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-22 09:05:22 +0200

    upd second work

--- secondwork/src/index.js
+++ secondwork/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import {createRoot} from "react-dom/client";
 const root=createRoot(document.querySelector('#root'));
+
+const component=(<header>
+    Test
+</header>)
+
+
+
+
+
+
 root.render(
-    <div>
+    <div>{component}
         Hello
     </div>
 )
