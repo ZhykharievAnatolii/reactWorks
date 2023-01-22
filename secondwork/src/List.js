46faf2dd96b62865ee5bf710658ad2b26a02196a
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-22 20:46:45 +0200

    upd markup

--- secondwork/src/List.js
+++ secondwork/src/List.js
@@ -1,10 +1,28 @@
 import React from "react";
+import {Listitem} from "./Listitem";
 
 export const List = () => {
-  return(
-      <div className="container">
-          <div className="row">
 
+    const data=[
+        {
+            id:2,
+            title:'First',
+
+        },
+        {
+            id:4,
+            title:'Second',
+
+        }
+    ]
+  return(
+      <div className="container mt-4">
+          <div className="row">
+              {
+                  data.map(({id,title})=>{
+                      return <Listitem key={id} id={id} title={title}/>
+                  })
+              }
           </div>
       </div>
   )
--- secondwork/src/Listitem.js
+++ secondwork/src/Listitem.js
@@ -1,9 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export function Listitem({id,title}) {
     return(
-        <li>
+        <li className="col-3 offset-1">
             {`${id}-${title}`}
         </li>
     )
+}
+
+Listitem.propTypes={
+    id:PropTypes.number.isRequired,
+    title:PropTypes.string.isRequired
 }
